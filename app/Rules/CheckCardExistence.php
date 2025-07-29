bee710a6b565b93b48b67412ae78679d38e84843
Buferxon <buferxon@gmail.com>
2025-07-29 08:24:19 -0500

    Ajustes para verificar que no se registre dos veces una tarjeta

--- app/Rules/CheckCardExistence.php
+++ app/Rules/CheckCardExistence.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Support\Facades\DB;
 use App\Helpers\CardHelper;
+use App\Models\Card;
 
 class CheckCardExistence implements ValidationRule
 {
@@ -22,6 +23,15 @@ class CheckCardExistence implements ValidationRule
 
         if (empty($reg_card)) {
             $fail('La tarjeta con el serial interno proporcionado no existe o no cumple con los criterios.');
+        }
+
+        $card = Card::where('ISS_ID', $reg_card->iss_id ?? null)
+            ->where('CD_ID', $reg_card->cd_id ?? null)
+            ->where('CRD_SNR', $reg_card->crd_snr ?? null)
+            ->first();
+
+        if (!empty($card)) {
+            $fail('La tarjeta con el serial interno proporcionado ya se encuentra registrada.');
         }
     }
 }
